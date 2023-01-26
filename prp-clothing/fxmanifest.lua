60ca4ad624fb63012f19a3492a8e994434a1de85
ThatBitxhBecca <rebeccapelks@gmail.com>
2023-01-26 13:02:11 -0600

    Beta bandaid for clothing

--- prp-clothing/fxmanifest.lua
+++ prp-clothing/fxmanifest.lua
@@ -2,7 +2,9 @@ fx_version 'cerulean'
 game 'gta5'
 
 files {
-    'mp_f_freemode_01_prolific.meta'
+    'mp_f_freemode_01_prolific.meta',
+    'mp_m_freemode_01_prolific.meta'
 }
 
 data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_prolific.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_prolific.meta'
