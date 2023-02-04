baf0d3f9ca4514fee334205d5f0ef5d889a630f9
ThatBitxhBecca <rebeccapelks@gmail.com>
2023-02-04 14:34:20 -0600

    Last of the clothing before launch

--- prp-clothing/fxmanifest.lua
+++ prp-clothing/fxmanifest.lua
@@ -4,7 +4,9 @@ game 'gta5'
 files {
     'mp_f_freemode_01_prolific.meta',
     'mp_m_freemode_01_prolific.meta'
+ 
 }
 
 data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_prolific.meta'
 data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_prolific.meta'
+
